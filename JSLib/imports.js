753d9d6bf0be440328e239579c6bfe7c9cd8c0ef
dylan zhang <dylan.zhangzhi@gmail.com>
2013-03-30 12:04:00 +0800

    delete JSLib

--- JSLib/imports.js
+++
@@ -1,8 +0,0 @@
-/**
- *引入封装的各功能模块
- */
-#import "lang-ext.js"
-#import "log.js"
-#import "assertion.js"
-#import "automatioin-extend.js"
-#import "appTest.js"
--- JSLib/lang-ext.js
+++
@@ -1,9 +0,0 @@
-/**
- * Extend +destination+ with +source+
- */
-function extend(destination, source) {
-  for (var property in source) {
-    destination[property] = source[property];
-  }
-  return destination;
-};
